d7592996667b2332540a009d7fcc4982a4c4983e
IrminaDs <irminads@student.agh.edu.pl>
2025-10-14 18:57:37 +0200

    PLAYER WORKS

--- addons/godot-xr-tools/hands/hand.gd
+++ addons/godot-xr-tools/hands/hand.gd
@@ -157,9 +157,14 @@ func _physics_process(_delta: float) -> void:
 	if _controller:
 		var grip : float = _controller.get_float(grip_action)
 		var trigger : float = _controller.get_float(trigger_action)
+		
+		print("Grip action:", grip_action, "Trigger action:", trigger_action)
+		print("Grip value:", _controller.get_float(grip_action), "Trigger value:", _controller.get_float(trigger_action))
 
 		# Allow overriding of grip and trigger
-		if _force_grip >= 0.0: grip = _force_grip
+		if _force_grip >= 0.0: 
+			grip = _force_grip 
+			print("A")
 		if _force_trigger >= 0.0: trigger = _force_trigger
 
 		$AnimationTree.set("parameters/Grip/blend_amount", grip)
