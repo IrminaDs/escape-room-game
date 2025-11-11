48aa0be6adce496e4ae800a475af8a7733ec08b5
IrminaDs <irminads@student.agh.edu.pl>
2025-11-11 13:22:24 +0100

    better disk

--- scripts/room1/disk_controller.gd
+++ scripts/room1/disk_controller.gd
@@ -2,11 +2,16 @@ extends Node
 
 @onready var parent_pickable = get_parent()
 @onready var inner_disk = $"../CSGCombiner3D/InnerDisk"
+@onready var label = $"../Label3D"
 @onready var left_controller: XRController3D = get_tree().get_current_scene().get_node("Player/XROrigin3D/LeftController")
 @onready var right_controller: XRController3D = get_tree().get_current_scene().get_node("Player/XROrigin3D/RightController")
 
 var is_held = false
-var rotation_speed = 45.0
+var offset := 0 
+var alphabet_size := 26
+var step := deg_to_rad(360.0 / 26)
+var left_was_pressed := false
+var right_was_pressed := false
 
 const ROTATE_LEFT_ACTION = "rotate_left"
 const ROTATE_RIGHT_ACTION = "rotate_right"
@@ -14,6 +19,7 @@ const ROTATE_RIGHT_ACTION = "rotate_right"
 func _ready():
 	parent_pickable.connect("picked_up", Callable(self, "_on_picked_up"))
 	parent_pickable.connect("dropped", Callable(self, "_on_dropped"))
+	_update_label()
 
 func _on_picked_up(_by):
 	is_held = true
@@ -24,8 +30,22 @@ func _on_dropped(_by):
 func _process(delta):
 	if not is_held:
 		return
+	
+	var left_pressed = left_controller.is_button_pressed("trigger")
+	var right_pressed = right_controller.is_button_pressed("trigger")
+		
+	if left_pressed and not left_was_pressed:
+		_rotate_disk(1)
+	elif right_pressed and not right_was_pressed:
+		_rotate_disk(-1)
+	
+	left_was_pressed = left_pressed
+	right_was_pressed = right_pressed
 
-	if left_controller.is_button_pressed("trigger"):
-		inner_disk.rotate_y(deg_to_rad(rotation_speed * delta))
-	elif right_controller.is_button_pressed("trigger"):
-		inner_disk.rotate_y(deg_to_rad(-rotation_speed * delta))
+func _rotate_disk(direction: int):
+	inner_disk.rotate_y(step * direction)
+	offset = (offset + direction + alphabet_size) % alphabet_size
+	_update_label()
+
+func _update_label():
+	label.text = "%d" % [offset]
