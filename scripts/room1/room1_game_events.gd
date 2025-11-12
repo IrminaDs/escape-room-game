38abb0418b7e5e08e332c5e34894cc4f4d70f478
IrminaDs <irminads@student.agh.edu.pl>
2025-11-12 14:31:53 +0100

    clock lock

--- scripts/room1/room1_game_events.gd
+++ scripts/room1/room1_game_events.gd
@@ -4,3 +4,4 @@ signal chest_unlocked
 signal desk_unlocked
 signal book_opened
 signal ceasar_placed
+signal clock_lock
--- scripts/room_1.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@onready var fade_rect = $CanvasLayer/ColorRect
-
-func _ready():
-	fade_rect.modulate.a = 1.0  # całkowicie czarny na start
-	var tween = create_tween()
-	tween.tween_property(fade_rect, "modulate:a", 0.0, 1.5).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
