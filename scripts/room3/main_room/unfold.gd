56719e6551617245552b0ca4d448ff21b7f38faa
zsliwa <160980048+zsliwa@users.noreply.github.com>
2025-12-11 14:49:19 +0100

    PaperBall into RSA note, but note falls down.
    Epic. But broken.

--- scripts/room3/main_room/unfold.gd
+++ scripts/room3/main_room/unfold.gd
@@ -1,69 +1,73 @@
 extends Node
 
-@export var pickable: XRToolsPickable
-@export var sheet_scene: PackedScene
-@export var trigger_threshold := 0.75
-@export var stretch_threshold := 0.35
+@onready var parent_pickable = get_parent()
+@onready var left_controller: XRController3D = get_tree().get_current_scene().get_node("Player/XROrigin3D/LeftController")
+@onready var right_controller: XRController3D = get_tree().get_current_scene().get_node("Player/XROrigin3D/RightController")
 
-var left_hand: XRController3D = null
-var right_hand: XRController3D = null
-var unfolded := false
+var is_held = false
+var triggers_pressed = false
+var initial_distance: float = 0.0
+var transformed = false
 
+var left_was_pressed = false
+var right_was_pressed = false
+
+const DISTANCE_THRESHOLD := 0.25
+
+var paper_sheet_scene: PackedScene = preload("res://scenes/room3/equipment/main_room/rsa.tscn")
 
 func _ready():
-	if not pickable:
-		pickable = get_parent() as XRToolsPickable
+	parent_pickable.connect("picked_up", Callable(self, "_on_picked_up"))
+	parent_pickable.connect("dropped", Callable(self, "_on_dropped"))
 
-	pickable.grabbed.connect(_on_grabbed)
+func _on_picked_up(_by):
+	is_held = true
 
-func _on_grabbed(controller: XRController3D, by_handle: bool):
-	if controller.is_left_hand():
-		left_hand = controller
+func _on_dropped(_by):
+	is_held = false
+	triggers_pressed = false
+	initial_distance = 0.0
+
+func _process(delta):
+	if transformed or not is_held:
+		return
+
+	var left_pressed = left_controller.is_button_pressed("trigger")
+	var right_pressed = right_controller.is_button_pressed("trigger")
+
+	if left_pressed and not left_was_pressed:
+		triggers_pressed = false
+	if right_pressed and not right_was_pressed:
+		triggers_pressed = false
+
+	if left_pressed and right_pressed:
+		if not triggers_pressed:
+			triggers_pressed = true
+			initial_distance = left_controller.global_transform.origin.distance_to(right_controller.global_transform.origin)
 	else:
-		right_hand = controller
+		triggers_pressed = false
+		initial_distance = 0.0
 
+	if triggers_pressed:
+		var current_distance = left_controller.global_transform.origin.distance_to(right_controller.global_transform.origin)
+		if current_distance - initial_distance > DISTANCE_THRESHOLD:
+			_transform_to_sheet()
 
-func _on_released(controller: XRController3D, by_handle: bool):
-	if controller == left_hand:
-		left_hand = null
-	elif controller == right_hand:
-		right_hand = null
+	left_was_pressed = left_pressed
+	right_was_pressed = right_pressed
 
-
-func _physics_process(_delta):
-	if unfolded:
+func _transform_to_sheet():
+	if transformed:
 		return
+	transformed = true
 
-	if not (left_hand and right_hand):
-		return
+	var sheet = paper_sheet_scene.instantiate() as Node3D
+	sheet.scale = Vector3.ONE
+	get_tree().get_current_scene().add_child(sheet)
 
-	var tl := left_hand.get_float("trigger")
-	var trir := right_hand.get_float("trigger")
+	sheet.global_transform = parent_pickable.global_transform
 
-	if tl < trigger_threshold or trir < trigger_threshold:
-		return
+	if sheet.has_signal("picked_up"):
+		sheet.emit_signal("picked_up", null)
 
-	var dist := left_hand.global_position.distance_to(right_hand.global_position)
-
-	if dist > stretch_threshold:
-		unfold()
-
-
-func unfold():
-	if unfolded:
-		return
-	unfolded = true
-
-	var tween = pickable.create_tween()
-	tween.tween_property(pickable, "scale", pickable.scale * 2, 0.20)\
-		.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-
-	tween.finished.connect(_spawn_sheet)
-
-
-func _spawn_sheet():
-	var sheet = sheet_scene.instantiate()
-	sheet.global_transform = pickable.global_transform
-	pickable.get_parent().add_child(sheet)
-
-	pickable.queue_free()
+	parent_pickable.queue_free()
