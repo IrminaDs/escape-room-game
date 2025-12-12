e58d5678fec79e226929ad25004e4fef4d5be337
zsliwa <160980048+zsliwa@users.noreply.github.com>
2025-12-12 13:13:15 +0100

    Notepad grab animation

---
+++ scripts/room3/main_room/notepad.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@onready var parent_node = get_parent()
+@onready var anim_player: AnimationPlayer = $"../Sketchfab_Scene/AnimationPlayer"
+
+var first_grab = true
+
+func _ready():
+	if parent_node.has_signal("grabbed"):
+		parent_node.connect("grabbed", Callable(self, "_on_grabbed"))
+	if parent_node.has_signal("released"):
+		parent_node.connect("released", Callable(self, "_on_released"))
+
+func _on_grabbed(pickable, by):
+	if pickable != parent_node:
+		return
+
+	anim_player.play("Scene")
+
+func _on_released(pickable, by):
+	if pickable != parent_node:
+		return
+
+	anim_player.play_backwards()
