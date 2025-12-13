891546ce39d89e152863df87e4ef50bd6a421f25
IrminaDs <irminads@student.agh.edu.pl>
2025-12-13 12:48:18 +0100

    better menu

--- scripts/set_player.gd
+++ scripts/set_player.gd
@@ -1,14 +1,17 @@
 extends Node
 
 
+@export var position: Vector3
+@export var degrees: int
+
 func _ready() -> void:	
 	var player = get_node("/root/Main/Player")
 	var origin = player.get_node("XROrigin3D")
 	var body = origin.get_node("PlayerBody")
 
 	var t = Transform3D()
-	t.origin = Vector3(0, 0, -0.25)
-	t.basis = Basis(Vector3.UP, deg_to_rad(180))
+	t.origin = position
+	t.basis = Basis(Vector3.UP, deg_to_rad(degrees))
 	body.teleport(t)
 
 func _process(delta: float) -> void:
