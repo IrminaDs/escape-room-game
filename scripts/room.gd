2560891a3785713355bf5c66a71e3062f150f333
zsliwa <160980048+zsliwa@users.noreply.github.com>
2025-12-13 13:16:20 +0100

    From 'master'

--- scripts/room.gd
+++ scripts/room.gd
@@ -5,21 +5,14 @@ extends Node3D
 @onready var contr_right = get_tree().get_current_scene().get_node("Player/XROrigin3D/RightController")
 
 func _ready():
-	var player = get_node("/root/Main/Player")
 	var point_l = contr_left.get_node("FunctionPointer")
 	var point_r = contr_right.get_node("FunctionPointer")
 	var move_l = contr_left.get_node("MovementDirect")
 	var move_r = contr_right.get_node("MovementTurn")
-	var light = player.get_node("SpotLight3D")
+	var light = get_tree().get_current_scene().get_node("Player/SpotLight3D")
 	
-	point_l.visible = true
-	point_r.visible = true
-	move_l.enabled = false
-	move_r.enabled = false
-	light.visible = true
-	
-	var audio = $AudioStreamPlayer
-	var stream = load("res://models/menu/sounds/Ossuary 6 - Air.mp3")
-	stream.loop = true
-	audio.stream = stream
-	audio.play()
+	point_l.visible = false
+	point_r.visible = false
+	move_l.enabled = true
+	move_r.enabled = true
+	light.visible = false
