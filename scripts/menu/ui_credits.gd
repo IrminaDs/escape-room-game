de755610129f96bc38e0e8ec1e5615f02003e352
IrminaDs <irminads@student.agh.edu.pl>
2026-01-17 13:01:41 +0100

    working (this time im sure) export

--- scripts/menu/ui_credits.gd
+++ scripts/menu/ui_credits.gd
@@ -5,9 +5,12 @@ extends CanvasLayer
 
 @onready var texts = $Texts
 
-func _ready() -> void:
+func update_elements():
 	for child in texts.get_children():
 		child.visible = false
 	
 	if texts.has_node(element):
 		texts.get_node(element).visible = true
+
+func set_element(value: String) -> void:
+	element = value
